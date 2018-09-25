5995b1c6fe3a3265cc04bfcfbbcda48f22ae40c2
Kyle Bateman <kyle@batemans.org>
2018-09-25 16:24:17 -0600

    State machine for new tally working

--- sample/tally_start.sql
+++ sample/tally_start.sql
@@ -1,10 +1,13 @@
 --Initiate a tally for user 10001
 
-delete from mychips.tallies where tally_ent = 10001 and status in ('void', 'draft');
+--delete from mychips.tallies where status in ('void', 'draft');
+delete from mychips.tallies;
 
 insert into mychips.tallies
   (tally_ent, tally_guid, partner, user_sig)
 values
-  (10001, '18d44de5-837d-448f-8d96-9136372987cf',10002,'User signature');
+  (10001, '18d44de5-837d-448f-8d96-9136372987cf',10000,'Adam signature');
 
 update mychips.tallies set request = 'draft' where tally_ent = 10001 and status = 'void';
+
+select user_cdi, part_cdi, tally_type, state from mychips.tallies_v;
