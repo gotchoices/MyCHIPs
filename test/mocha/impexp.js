2aee9f784cae16bc212a8ec1d70fc660a1bea768
Kyle Bateman <kyle@lux2.batemans.org>
2018-10-09 14:05:29 -0600

    Work: logging to separate logfiles in separate windows

--- test/mocha/impexp.js
+++ test/mocha/impexp.js
@@ -3,7 +3,7 @@
 //- 
 
 const assert = require("assert");
-const DatabaseName = "kyle"			//"mychipsTestDB"
+const DatabaseName = require('../database')
 var fs = require('fs')
 var log = require('../../lib/logger')('testImpex')
 var { dbClient } = require("wyseman")
@@ -14,35 +14,50 @@ const dbConfig = {
   schema: __dirname + "/../../lib/schema.sql"
 }
 
+function dbAndCheck(sqlFile, db, done, check) {
+  fs.readFile(sqlFile, (err, fileData) => {
+    if (err) done(err)
+    var jsonData = JSON.parse(fileData)
+    db.query("select json.import($1::jsonb) as record;", [jsonData] ,(err, res) => {
+      if (err) done(err)
+      check(res,res.rows[0].record.slice(1,-1).split(','))
+    })
+  })
+}
+
 describe("JSON import/export", function() {
   var db
 
-  before('Connect to (or create) test catabase', function(done) {
+  before('Connect to (or create) test database', function(done) {
     db = new dbClient(dbConfig, (chan, data) => {
       log.trace("Notify from channel:", chan, "data:", data)
-    }, ()=>{
-      log.trace("Connected")
-      done()
+    }, ()=>{log.trace("Connected"); done()})
+  })
+
+  before('Delete all test users if there are any', function(done) {
+    db.query("delete from base.ent where id >= $1;", [100] ,(err, res) => {done()})
+  })
+
+  it("Import Chips test organization", function(done) {
+    dbAndCheck(__dirname + "/../sample/org.json", db, done, function(res, row) {
+      assert.equal(row[0],100); done()
     })
   })
 
-  it("Import", function(done) {
-    fs.readFile(__dirname + "/../sample/org.json", (err, fileData) => {
-      if (err) throw err;
-      var jsonData = JSON.parse(fileData)
-//      var jsonData = JSON.parse('{"user":1234}')
-console.log("JSON:", jsonData)
-      db.query("select json.import($1::jsonb);", [jsonData] ,(err, res) => {
-console.log("Err:", err, "Res:", res)
-        assert.equal(true,true)
-        done()
-      })
+  it("Import user 1", function(done) {
+    dbAndCheck(__dirname + "/../sample/user1.json", db, done, function(res, row) {
+      assert.equal(row[0],10000); done()
+    })
+  })
+  it("Import user 2", function(done) {
+    dbAndCheck(__dirname + "/../sample/user2.json", db, done, function(res, row) {
+      assert.equal(row[0],10001); done()
     })
   })
 
   after('Disconnect from test database', function(done) {
-//    server2.close(serv1)
-    db.disconnect
+console.log("After:")
+    db.disconnect()
     done()
   })
 });
--- test/mocha/peer.js
+++ test/mocha/peer.js
@@ -13,51 +13,82 @@
 //- Create similar testing for chit flowchart
 //- 
 
-const port1 = 65431
-const port2 = 65432
-const host1 = "server1"
-const host2 = "server2"
 const assert = require("assert");
 const PeerCont = require("../../lib/peer")
-const DatabaseName = "mychipsTestDB"
+const DatabaseName = require('../database')
+const MachineIP = "192.168.57.101"
+const Uport=43210
+const Pport0=65430
+const Pport1=65431
+const Host0 = "server0"
+const Host1 = "server1"
 var log = require('../../lib/logger')('testpeer')
 var { dbClient } = require("wyseman")
-const dbConfig = {
-  database: DatabaseName,
-  listen: "DummyChannel",		//Force immediate connection to our DB
-  logger: log,
-  schema: __dirname + "/../../lib/schema.sql"
-}
 
-describe.skip("Peer to peer tallies", function() {
+//Initialize users
+const sqlUserInit = `begin; \n\
+  update mychips.users set user_inet = '${MachineIP}', user_port=${Uport} where user_inet isnull; \n\
+  update mychips.users_v set peer_inet = '${MachineIP}', peer_port=${Pport0}, host_id='${Host0}' where peer_ent = 10000; \n\
+  update mychips.users_v set peer_inet = '${MachineIP}', peer_port=${Pport1}, host_id='${Host1}' where peer_ent = 10001; \n\
+  select count(*) as count from mychips.users_v where id >= 10000; \n\
+  commit;`
+
+//Initiate a tally for user 10001
+const sqlTallyStart = "begin; \n\
+  delete from mychips.tallies; \n\
+  insert into mychips.tallies (tally_ent, tally_guid, partner, user_sig) values (10001, '18d44de5-837d-448f-8d96-9136372987cf',10000,'Adam signature'); \n\
+  update mychips.tallies set request = 'draft' where tally_ent = 10001 and status = 'void' returning status;	-- Only update (not insert) triggers will generate requests \n\
+  commit;"
+
+describe("Peer to peer tallies", function() {
   var server1, server2
-  var db
+  var db, dbc
 
-  before('Connect to (or create) test catabase', function(done) {
-    db = new dbClient(dbConfig, (chan, data) => {
-      log.trace("Notify from channel:", chan, "data:", data)
-    }, ()=>{
-      log.trace("Connected")
-      done()
-    })
+  before('Connect to (or create) test database', function(done) {
+    var conf = {database: DatabaseName, listen: 'ForceOpen', logger:log}
+    db = new dbClient(conf, (chan, data) => {
+      log.info("Notify from channel:", chan, "data:", data)
+    }, ()=>{log.debug("Connected"); done()})
   })
 
   before('Launch two peer servers', function() {
-    server1 = new PeerCont(port1, host1, dbConfig)
-//    server2 = new PeerCont(port2, host2, dbConfig)
+    server0 = new PeerCont(Pport0, Host0, {database: DatabaseName})
+    server1 = new PeerCont(Pport1, Host1, {database: DatabaseName})
   })
 
-  it("Propose tally", function(done) {
-    db.query("select count(*) from mychips.users_v", null, (err, res) => {
-console.log("Err:", err, "Res:", res)
-      assert.equal(true,true)
+  it("Check for correct number of test users", function(done) {
+    db.query(sqlUserInit, null, (err, res) => { if (err) done(err)
+      var count = res[4].rows[0]['count']
+//console.log("Users:", count)
+      assert.equal(count,2)
       done()
     })
   })
 
+  it("User 10001 proposes a tally to user 10000", function(done) {
+    db.query(sqlTallyStart, null, (err, res) => { if (err) done(err)
+      var stat = res[3].rows[0]['status']
+      log.debug("SQL Done:", stat)
+      assert.equal(stat, 'void')
+    })
+
+    var conf = {database: DatabaseName, listen: 'mychips_user_10000'}
+    dbc = new dbClient(conf, (chan, data) => {
+      log.info("Notify from channel:", chan, "data:", data)
+      var msg = JSON.parse(data)
+      log.trace("foil:", msg.foil)
+      assert.equal(msg.foil, 'james_madison.chip')
+      log.trace("signed.foil:", msg.signed.foil)
+//      assert.equal(msg.signed.foil, null)
+      done()
+    }, ()=>{log.debug("Temp client connected")})
+  })
+
   after('Disconnect from test database', function(done) {
-//    server2.close(serv1)
-    db.disconnect
+    dbc.disconnect()
+    db.disconnect()
+    server0.close()
+    server1.close()
     done()
   })
 });
