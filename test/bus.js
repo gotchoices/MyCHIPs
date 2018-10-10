2bff1d82600c771130024214c2b24c44e7f353f5
Kyle Bateman <kyle@lux2.batemans.org>
2018-10-09 18:07:55 -0600

    Basic testing of tallies working

---
+++ test/bus.js
@@ -0,0 +1,24 @@
+//Message bus
+//Allow a context to register itself to receive notification from a different async context
+// -----------------------------------------------------------------------------
+
+module.exports = function(name) {
+  this.name = name
+  this.clients = {}
+//console.log("Bus construct", name)
+
+  this.register = function(id, cb) {		//Clients register to receive callbacks
+//console.log("Bus register:",name, id, cb)
+    if (cb) 
+      this.clients[id] = cb			//Register
+    else if (id in this.clients && !cb)
+      delete this.clients[id]			//De-register
+  }
+    
+  this.notify = function(data) {
+//console.log("Bus notify:", name, this.clients)
+    Object.keys(this.clients).forEach(key => {
+        this.clients[key](data)
+    })
+  }
+}
