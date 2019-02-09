784fe980a5c3ae6186c20ae55c04b498ba49f3fe
Kyle Bateman <kyle@lux2.batemans.org>
2019-02-09 15:02:34 -0700

    Support for authenticated login

--- bin/mychips.js
+++ bin/mychips.js
@@ -12,46 +12,77 @@
 //-   Public document http server
 //- Can't start on fresh database with peerPort enabled (2 simultaneous attempts to create database)
 //- 
-var log = require('../lib/logger')('mychips')
-const OS = require('os')
-const Express = require('express')
-var expSPApp = Express()
+var	log		= require('../lib/logger')('mychips')
+const	Fs		= require('fs')
+const	OS		= require('os')
+const	Express		= require('express')
+const	Http		= require('http')
+const	Https		= require('https')
+var	expSPApp	= Express()
+var	credentials
+var	dbConf
 
 var argv = require('yargs')
-  .alias('h','hostID')    .default('hostID',    null)		//If peer servers run on multiple hosts, this identifies our host
-  .alias('s','spaPort')	  .default('spaPort',   8000)		//Serve client SPA's at this port
-  .alias('w','wgiPort')	  .default('wgiPort',   8001)		//Serve wysegi SPA at this port
-  .alias('u','userPort')  .default('userPort',  43210)		//User client at this port
-  .alias('a','adminPort') .default('adminPort', 54320)		//Admin client at this port
-  .alias('p','peerPort')  .default('peerPort',  65430)		//Peer-to-peer connections at this port
-  .alias('l','lifts')     .default('lifts', false)		//Run lift scheduler
-  .alias('m','model')     .default('model', false)		//Run agent-based model
-  .argv;
+  .alias('h','hostID')     .default('hostID',     null)		//If peer servers run on multiple hosts, this identifies our host
+  .alias('s','spaPort')	   .default('spaPort',    8000)		//Serve client SPA's at this port
+  .alias('u','userPort')   .default('userPort',   43210)	//User client at this port
+  .alias('a','adminPort')  .default('adminPort',  54320)	//Admin client at this port
+  .alias('p','peerPort')   .default('peerPort',   65430)	//Peer-to-peer connections at this port
+  .alias('k','serverKey')  .default('serverKey',  null)		//Private SSL key for this server
+  .alias('c','serverCert') .default('serverCert', null)		//SSL Certificate for this server
+  .alias('D','dbName')	   .default('dbName',     'mychips')	//Database name
+  .alias('A','dbAdmin')	   .default('dbAdmin',    'admin')	//Database administrator name
+  .alias('H','dbHost')	   .default('dbHost',     null)		//Database administrator name
+  .alias('w','wysegi')	   .default('wysegi',     false)	//Serve wysegi SPA
+  .alias('l','lifts')      .default('lifts',      false)	//Run lift scheduler
+  .alias('m','model')      .default('model',      false)	//Run agent-based model
+  .alias('n','noSSL')      .default('noSSL',      false)	//Do not run server with SSL
+  .argv
+
+if (!argv.noSSL) {
+  let serverKey = argv.serverKey || process.env.MYCHIPS_SERVER_KEY || 'pki/server_private_key.pem'
+    , serverCert = argv.serverCert  || process.env.MYCHIPS_SERVER_CERT || 'pki/server_certificate.pem'
+    , key = Fs.existsSync(serverKey) ? Fs.readFileSync(serverKey) : null
+    , cert = Fs.existsSync(serverCert) ? Fs.readFileSync(serverCert) : null
+  if (!key && !cert) log.error("Problem initializing SSL:", serverKey, serverCert)
+  credentials = {key, cert}
+}
+dbConf = {database:argv.dbName, user: argv.dbAdmin, host: argv.dbHost}
 
 log.trace("Host ID:    ", argv.hostID)
-log.trace("SPA Port:   ", argv.spaPort)
-log.trace("Wysegi Port:", argv.wgiPort)
+log.trace("SPA Port:   ", argv.spaPort, argv.wysegi, argv.serverKey, argv.serverCert)
 log.trace("Admin Port: ", argv.adminPort)
 log.trace("User Port:  ", argv.userPort)
 log.trace("Peer Port:  ", argv.peerPort)
 log.trace("Agent:", argv.model, "Lifts:", argv.lifts)
+log.trace("dbConf:", dbConf)
 
-if (Boolean(argv.spaPort)) {				//Create http server for client SPAs
-    expSPApp.use(Express.static('pub'))
-    expSPApp.listen(argv.spaPort)
-    log.debug("Serving client SPA's at port:", argv.spaPort)
-}
+if (Boolean(argv.spaPort)) {				//Serve up client SPAs
+  let server = credentials ? Https.createServer(credentials, expSPApp) : Http.createServer(expSPApp)
 
-if (Boolean(argv.wgiPort)) {				//Create http server for wysegi SPA
-    app = Express();
-    app.use(Express.static('node_modules/wylib/dist'))
-    app.listen(argv.wgiPort)
-    log.debug("Serving wysegi SPA at port:", argv.wgiPort)
+  expSPApp.use(Express.static('pub'))
+  if (Boolean(argv.wysegi))
+    expSPApp.use(Express.static('node_modules/wylib/dist'))	//Serve up wylib wysegi app too
+  expSPApp.get('/clientip', (req, res)=>{		//Let clients know their IP number for validation purposes
+//log.trace("Client request:", req.headers)
+    date = new Date
+    res.send(JSON.stringify({
+      ip: req.connection.remoteAddress,
+      real: req.headers['x-real-ip'] || req.headers['x-forwarded-for'],
+      cookie: req.headers.cookie,
+      userAgent: req.headers['user-agent'],
+      date,
+    }))
+  })
+
+  server.listen(argv.spaPort)
+  log.debug("Serving client SPA's at port:", argv.spaPort)
 }
 
 if (Boolean(argv.adminPort)) {				//Create socket server for admin data
   const AdminCont = require('../lib/admin.js')		//Admin client connection controller
-  var admin = new AdminCont(argv.adminPort, expSPApp)
+  let wsConf = {port: argv.adminPort, credentials, expApp:expSPApp}
+  var admin = new AdminCont(dbConf, wsConf)
 }
 
 //if (Boolean(argv.userPort)) {				//Create socket server for user data
--- test/settings.js
+++ test/settings.js
@@ -1,6 +1,9 @@
 module.exports={
   DatabaseName: "mychipsTestDB",
   MachineIP: "192.168.57.101",
+  DBAdmin: "admin",
+  AdminID: "r1",
   UserPort: 43210,
-  PeerPort: 65432
+  AdminPort: 54320,
+  PeerPort: 65432,
 }
