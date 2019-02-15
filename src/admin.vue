8cbd21e71a5ef84f38c857274b7801f5a0a7ed00
Kyle Bateman <kyle@lux2.batemans.org>
2019-02-15 14:05:36 -0700

    Last commit before code moved into admin.js

--- src/admin.vue
+++ src/admin.vue
@@ -5,37 +5,59 @@
 //- Pull application text from database
 //- 
 <template>
-  <wylib-app tag="mychips_admin" title="MyCHIPs Administration" :db="dbConf" :state="state" :tabs="tabs" :current="state.curTab" @tab="(t)=>{state.curTab = t}">
-    <keep-alive><component :is="'app-' + state.curTab" :state="state.tabs[state.curTab]"/></keep-alive>
+  <wylib-app :tag="tag" :db="dbConf" :title="title" :state="state" :tabs="tabs" :current="state.curTab" @tab="(t)=>{state.curTab = t}">
+    <component v-for="t in tabs" :key="t.tag" v-show="curTab==t.tag" v-if="hasRun[t.tag]" :is="components[t.tag]" :tag="t.tag" :view="views[t.tag]" :state="state.tabs[t.tag]"/>
   </wylib-app>
 </template>
 
 <script>
 import Wylib from 'wylib'
-import AppConts from './admin/conts.vue'
-import AppUsers from './admin/users.vue'
-import AppURNet from './admin/urnet.vue'
-import AppConfig from './admin/config.vue'
+import AppURNet from './urnet.vue'
 
 export default {
-  components: {'wylib-app': Wylib.Application, 'app-users': AppUsers, 'app-conts': AppConts, 'app-urnet': AppURNet, 'app-config': AppConfig},
+  components: {'wylib-app': Wylib.Application, 'wylib-launch': Wylib.Launcher, 'app-urnet': AppURNet},
   data() { return {
     state:      {curTab: 'users', tabs: {users:{}, conts:{}, urnet:{}, config:{}}},
-    tog:	true,
+    tag:	'mychips_admin',
+    title:	'MyCHIPs Admin',
+    dbConf:	"['mychips_admin','wylib']",
+    hasRun:	{},
     tabs:	[
-      {tag: 'users', title: 'Users'},
-      {tag: 'conts',  title: 'Contracts'},
-      {tag: 'urnet',  title: 'Network'},
-      {tag: 'config', title: 'Settings'},
+      {tag: 'users',  view: 'mychips.users_v', title: 'Users'},
+      {tag: 'conts',  view: 'mychips.contracts_v', title: 'Contracts'},
+      {tag: 'config', view: 'base.parm_v', title: 'Settings'},
+      {tag: 'urnet',  component: 'app-urnet', title: 'Network'},
     ],
-    dbConf: "['mychips_admin','wylib']"
   }},
+  computed: {
+    curTab() {
+      return this.state.curTab || 'users'
+    },
+    views() {
+      return this.tabs.reduce((ac,el)=>{
+        ac[el.tag] = el.view
+        return ac
+      }, {})
+    },
+    components() {
+      return this.tabs.reduce((ac,el)=>{
+        ac[el.tag] = el.component || 'wylib-launch'
+        return ac
+      }, {})
+    },
+  },
 //  methods: {
 //  },
-//  mounted: function () {
-//  },
+  mounted: function () {
+//console.log("Admin init:", this.curTab)
+    if (this.curTab)
+      this.$nextTick(()=>{this.$set(this.hasRun, this.curTab, true)})
+  },
+  watch: {
+    'state.curTab': function(newVal, oldVal) {
+//console.log("Watched curTab:", newVal)
+      this.$set(this.hasRun, this.curTab, true)
+    }
+  },
 }
 </script>
-
-<style>
-</style>
