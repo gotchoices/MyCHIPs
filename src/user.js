649f2fb614fac0dbe017917618ff1b9fb41f3e83
Kyle Bateman <kyle@lux0.batemans.org>
2019-10-03 20:44:01 -0600

    Get basic user UI working with latest library specs

--- src/user.js
+++ src/user.js
@@ -4,12 +4,61 @@
 // TODO:
 //- 
 import Vue from 'vue'
+import Wylib from 'wylib'
+import AppVBS from './visbs.vue'
 Vue.config.productionTip = false
 
-import User from './user.vue'
-
+const Template = `
+  <wylib-app :tag="tag" :db="dbConf" :title="title" :state="state" :tabs="tabs" :current="state.curTab" @tab="(t)=>{state.curTab = t}">
+    <component v-for="t in tabs" :key="t.tag" v-show="curTab==t.tag" v-if="hasRun[t.tag]" :is="components[t.tag]" :tag="t.tag" :view="views[t.tag]" :state="state.tabs[t.tag]"/>
+  </wylib-app>
+`
 new Vue({
   el: '#app',
-  template: '<User/>',
-  components: { User }
+  template: Template,
+  components: {'wylib-app': Wylib.Application, 'wylib-launch': Wylib.Launcher, 'app-visbs': AppVBS},
+  data() { return {
+    state:      {curTab: 'users', tabs: {users:{}, conts:{}, urnet:{}, config:{}}},
+    tag:	'mychips_user',
+    title:	'MyCHIPs User Portal',
+    dbConf:	['mychips_user','wylib'],
+    hasRun:	{},
+    tabs:	[
+      {tag: 'dash', component: 'app-visbs', title: 'Dashboard'},
+      {tag: 'peers', view: 'mychips.peers_v_me', title: 'Peers'},
+      {tag: 'tallies', view: 'mychips.tallies_v_me', title: 'Tallies'},
+      {tag: 'config', view: 'base.parm_v', title: 'Settings'},
+    ],
+    currentTab: 'dash'
+  }},
+  computed: {
+    curTab() {
+      return this.state.curTab || 'users'
+    },
+    views() {
+      return this.tabs.reduce((ac,el)=>{
+        ac[el.tag] = el.view
+        return ac
+      }, {})
+    },
+    components() {
+      return this.tabs.reduce((ac,el)=>{
+        ac[el.tag] = el.component || 'wylib-launch'
+        return ac
+      }, {})
+    },
+  },
+//  methods: {
+//  },
+  mounted: function () {
+//console.log("Admin init:", this.curTab)
+    if (this.curTab)
+      this.$nextTick(()=>{this.$set(this.hasRun, this.curTab, true)})
+  },
+  watch: {
+    'state.curTab': function(newVal, oldVal) {
+//console.log("Watched curTab:", newVal)
+      this.$set(this.hasRun, this.curTab, true)
+    }
+  },
 })
