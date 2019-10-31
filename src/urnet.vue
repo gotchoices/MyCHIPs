dd8d262b99f9d5aa751f76710c32db6329f9159f
Kyle Bateman <kyle@lux0.batemans.org>
2019-10-31 14:16:33 -0600

    Restructure network graph code, better async hopefully

--- src/urnet.vue
+++ src/urnet.vue
@@ -6,9 +6,9 @@
 //X- Update tallies when added, removed
 //X- updateNodes() can't handle a deleted node, can only add them
 //X- When we get a notice from the database, only load those nodes, tallies that have changed
-//- Make totals reactive, auto update when sums come in
-//- Tallies disappear if I delete them
-//- Hubs collapse down if I remove a tally under the stack
+//X- Make totals reactive, auto update when sums come in
+//X- Tallies disappear if I delete them
+//X- Hubs collapse down if I remove a tally under the stack
 //- 
 
 <template>
@@ -38,7 +38,7 @@ export default {
   }},
   computed: {
     totals: function() {
-console.log("Totals:", Object.keys(this.tallies).length)
+//console.log("Totals:", Object.keys(this.tallies).length)
       let tots = {}
       Object.keys(this.tallies).forEach(key=>{
         let debits = 0, credits = 0
@@ -51,14 +51,10 @@ console.log("Totals:", Object.keys(this.tallies).length)
   },
   methods: {
     peer(dat) {				//Generate SVG code for a user/peer node
-//console.log("User", dat.id, dat.peer_cdi, this.totals[dat.peer_cdi])
+console.log("User", dat.id, dat.peer_cdi, this.totals[dat.peer_cdi])
       let { id, std_name, peer_cdi, peer_sock, user_ent } = dat
-        , tots = peer_cdi in this.totals ? this.totals[peer_cdi] : {}
-        , debits = Math.round(tots.debits || 0)
-        , credits = Math.round(-tots.credits || 0)
-        , total =  Math.round(debits - credits)
-        , fColor = (total < 0 ? '#ff0000' : '#0000ff')
-        , sumLine = `${debits} - ${credits} = <tspan stroke="${fColor}" fill="${fColor}">${total}</tspan>`
+        , fColor = (dat.total < 0 ? '#ff0000' : '#0000ff')
+        , sumLine = user_ent ? `${dat.stock_tot} - ${-dat.foil_tot} = <tspan stroke="${fColor}" fill="${fColor}">${dat.total}</tspan>` : ''
         , yOff = this.fontSize + 3
         , host = peer_sock.split('.')[0]
         , cdiLine = `${peer_cdi}@${host}`
@@ -68,7 +64,7 @@ console.log("Totals:", Object.keys(this.tallies).length)
           <tspan x="4" y="${yOff * 2}">${cdiLine}</tspan>
           <tspan x="4" y="${yOff * 3}">${sumLine}</tspan>
         </text>`
-        , max = Math.max(cdiLine.length, std_name.length + 6, sumLine.length-44)	//take tspan into account
+        , max = Math.max(cdiLine.length + 2, std_name.length + 6, sumLine.length-48)	//take tspan into account
         , width = max * this.fontSize * 0.55
         , height = this.fontSize * 3.8
         , bColor = user_ent ? "#d0d0e4" : "#d0e4d0"
@@ -78,30 +74,77 @@ console.log("Totals:", Object.keys(this.tallies).length)
           ${text}
         </g>`
         , ends = [{x:width/2, y:0}, {x:width, y:height*0.5}, {x:width/2, y:height}, {x:0, y:height*0.5}]
-//console.log("User body:", width, height)
+//console.log("User body:", body, width, height)
       return {body, ends, width, height}
     },
 
+    updateLink(i, idx, cdi, dat) {
+      let node = this.state.nodes[cdi]					//Get node's state object
+        , guid = dat.guids[i]
+        , isFoil = (dat.types[i] == 'foil')
+        , amount = dat.totals[i]
+        , link = dat.part_cdis[i]
+        , inside = dat.insides[i]
+        , noDraw = (isFoil && inside)
+        , reverse = (isFoil && !inside)
+        , nodeLink = node.links.find(lk => (lk.index == guid))	//Do we already have a definition for this link?
+        , xOffset = node.width / 2
+        , yOffset = isFoil ? node.height + (this.hubHeight * (idx + 0.5)) : -this.hubHeight * (idx + 0.5)	//Stack it on top (stocks) or on bottom (foils)
+        , color = amount == 0 ? '#f0f0f0' : (amount < 0 ? '#F0B0B0' : '#B0B0F0')
+        , hubYRad = this.hubHeight/2, hubXRad = this.hubWidth/2
+        , ends = [{x:xOffset-hubXRad, y:yOffset}, {x:xOffset+hubXRad, y:yOffset}]
+        , center = {x:xOffset, y:yOffset}
+
+      if (!nodeLink) {				//Create new data structure for link, hubs
+        nodeLink = {index:guid, link, ends, center, noDraw:null, reverse:null, found:true, hub:null}
+        node.links.push(nodeLink)
+      }
+console.log("  link:", link, node, idx, cdi, amount, yOffset, nodeLink)
+      Object.assign(nodeLink, {ends, center, link, noDraw, reverse, found:true, hub: ()=>{
+        return `<g transform="translate(${center.x}, ${center.y})">
+          <ellipse rx="${hubXRad}" ry="${hubYRad}" stroke="black" stroke-width="1" fill="${color}"/>
+          <text y="${hubYRad/2}" text-anchor="middle" style="font:normal ${this.fontSize}px sans-serif;">${amount}</text>
+        </g>`
+      }})
+    },
+
     updateNodes(dTime) {
-      let where = [['peer_cdi', 'notnull']]
-        , spec = {view: 'mychips.users_v', fields: ['id', 'std_name', 'peer_cdi', 'peer_sock', 'user_ent'], where, order: 1}
-      if (dTime) where.push(['mod_date', '>=', dTime])
+      let where = [['peer_ent', 'notnull']]
+        , fields = ['id', 'std_name', 'peer_cdi', 'peer_sock', 'user_ent', 'total', 'stock_tot', 'foil_tot', 'tallies', 'types', 'totals', 'guids', 'part_cdis', 'insides']
+        , spec = {view: 'mychips.users_v_tallysum', fields, where, order: 1}
+      if (dTime) where.push(['latest', '>=', dTime])
+
       Wylib.Wyseman.request('urnet.peer.'+this._uid, 'select', spec, (data,err) => {
         let notFound = Object.assign({}, this.state.nodes)
-//console.log("Update nodes:", data.length, data)
-        for (let dat of data) {
-          let bodyObj = this.peer(dat)
+//console.log("Update nodes:", dTime, this.state.nodes, data.length, data)
+        for (let d of data) {
+          let bodyObj = this.peer(d)
             , radius = bodyObj.height / 2
-          if (dat.peer_cdi in this.state.nodes) {
-            Object.assign(this.state.nodes[dat.peer_cdi], bodyObj, {radius})
-//console.log("n Dat:", dat.peer_cdi, this.state.nodes[dat.peer_cdi].body)
+            , cdi = d.peer_cdi
+          if (cdi in this.state.nodes) {
+            Object.assign(this.state.nodes[cdi], bodyObj, {radius})
+//console.log("n Dat:", cdi, this.state.nodes[cdi].body)
           } else {
             let x = Math.random() * this.state.width/2
               , y = Math.random() * this.state.height/2
-            this.$set(this.state.nodes, dat.peer_cdi, Object.assign(bodyObj, {tag:dat.peer_cdi, x, y, radius, links:[], hubIdx: {stock: [], foil: []}}))
-//console.log("N Dat:", dat.peer_cdi, x, y, this.state.nodes[dat.peer_cdi].x)
+            this.$set(this.state.nodes, cdi, Object.assign(bodyObj, {tag:cdi, x, y, radius, links:[]}))
+//console.log("N Dat:", cdi, x, y)
           }
-          delete notFound[dat.peer_cdi]
+//console.log("Dat:", d)
+          let stocks = 0, foils = 0
+          for (let i = 0; i < d.tallies; i++) {
+            let idx = (d.types[i] == 'stock') ? stocks++ : foils++
+            this.updateLink(i, idx, cdi, d)
+          }
+          let node = this.state.nodes[cdi]
+          for (let i = node.links.length - 1; i >= 0; i--) {
+            let link = node.links[i]
+//console.log("  checking:", i, link, link.found)
+            if (!dTime && !link.found) node.links.splice(i,1)		//Delete if not found this iteration
+            link.found = false
+          }
+          
+          delete notFound[cdi]					//Note we processed this node
         }
 //console.log("Not found:", notFound, this.state.nodes)
         if (!dTime) Object.keys(notFound).forEach(key=>{	//Delete anything on the SVG, not now in nodes
@@ -110,67 +153,12 @@ console.log("Totals:", Object.keys(this.tallies).length)
       })
     },		//updateNodes
 
-    updateLinks(dTime) {
-      let spec = {view: 'mychips.tallies_v_graph', fields: ['guid', 'stock_cdi', 'foil_cdi', 'total'], where: [['state', '=', 'open']], order: [1,2]}
-        , procNode = (guid, cdi, part_cdi, total, type) => {
-            if (!this.state.nodes[cdi]) this.updateNodes()
-            let node = this.state.nodes[cdi]			//Get node's state object
-              , nodeLink = node.links.find(link => {return link.index == guid})	//Do we already have a definition for this link?
-              , idx = (tag => {					//Calculate an order 0..n for hub stacking
-                  let idx = node.hubIdx[type].findIndex(el=>(el == tag))
-                  if (idx < 0) node.hubIdx[type].push(tag)
-                  return idx > 0 ? idx : 0
-                })(guid)
-              , isFoil = type == "foil" ? true : false
-              , xOffset = node.width / 2
-              , yOffset = isFoil ? node.height + (this.hubHeight * (idx + 0.5)) : -this.hubHeight * (idx + 0.5)	//Stack it on top (stocks) or on bottom (foils)
-              , color = total == 0 ? '#f0f0f0' : (total < 0 ? '#F0B0B0' : '#B0B0F0')
-              , hubYRad = this.hubHeight/2, hubXRad = this.hubWidth/2
-              , ends = [{x:xOffset-hubXRad, y:yOffset}, {x:xOffset+hubXRad, y:yOffset}]
-              , center = {x:xOffset, y:yOffset}
-
-            if (!(cdi in this.tallies)) this.$set(this.tallies, cdi, {stock:[], foil:[]})
-            this.tallies[cdi][type].splice(idx, 1, total)
-console.log('Add to tallies:', idx, total)
-            if (!nodeLink) {				//Create new data structure for link, hubs
-              nodeLink = {index:guid, link:part_cdi, draw:isFoil, ends, center, found:null, hub:null}
-              node.links.push(nodeLink)
-            }
-//console.log("  node:", node, idx, cdi, part_cdi, nodeLink)
-            Object.assign(nodeLink, {ends, center, found:!dTime, hub: ()=>{
-              return `<g transform="translate(${center.x}, ${center.y})">
-                <ellipse rx="${hubXRad}" ry="${hubYRad}" stroke="black" stroke-width="1" fill="${color}"/>
-                <text y="${hubYRad/2}" text-anchor="middle" style="font:normal ${this.fontSize}px sans-serif;">${total}</text>
-              </g>`
-            }})
-          }		//procNode
-
-console.log("updateLinks:", dTime)
-      if (dTime) spec.where.push(['latest', '>=', dTime])
-      Wylib.Wyseman.request('urnet.tally.'+this._uid, 'select', spec, (data, e) => {
-        if (e) {console.log("Error in query:", e.message); return}
-        for (const dat of data) {				//For each tally link
-console.log("L Dat:", dat)
-          procNode(dat.guid, dat.stock_cdi, dat.foil_cdi, dat.total, 'stock')
-          procNode(dat.guid, dat.foil_cdi, dat.stock_cdi, -dat.total, 'foil')
-        }
-
-        if (!dTime) Object.values(this.state.nodes).forEach(node=>{	//Locate any tallies on the SVG, no longer in the DB
-          if (node.links) node.links.forEach((link, idx)=>{
-            if (!link.found) {node.links.splice(idx,1)}		//If not marked in previous iteration
-            link.found = false
-          })
-        })
-      })	//request
-    },		//updateLinks
-
-    refresh(dTime) {
-      this.updateNodes(dTime)
-      this.updateLinks(dTime)
+    refresh() {
+      this.updateNodes()
     },
     reset() {
       this.state.nodes = {}
-      this.refresh();
+      this.updateNodes()
     },
   },
 
@@ -179,16 +167,18 @@ console.log("L Dat:", dat)
     
     Wylib.Wyseman.listen('urnet.async.'+this._uid, 'mychips_admin', dat => {
 console.log("URnet async:", dat)
-      if (dat.target == 'peers') this.updateNodes(dat.time)
-      if (dat.target == 'tallies') this.updateLinks(dat.time)
-      if (dat.target == 'chits' && dat.oper == 'DELETE')	//Reload everything
-      	this.refresh()
+
+      if (dat.target == 'peers')
+        this.updateNodes(dat.oper == 'DELETE' ? null : dat.time)
+      if (dat.target == 'tallies')
+        this.updateNodes(dat.oper == 'DELETE' ? null : dat.time)
+//      if (dat.target == 'chits' && dat.oper == 'DELETE')	//Reload everything
+//      	this.refresh()
     })
   },
 
   mounted: function() {
     this.updateNodes()
-    this.updateLinks()
   }
 }
 </script>
