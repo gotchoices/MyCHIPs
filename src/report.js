56b7858afdc0d504a2ceaf0305cc4f7c1f1678de
Kyle Bateman <kyle@lux0.batemans.org>
2019-12-09 20:10:11 -0700

    Move most functionality to wylib/pop

--- src/report.js
+++ src/report.js
@@ -7,51 +7,8 @@ import Vue from 'vue'
 import Wylib from 'wylib'
 Vue.config.productionTip = false
 
-const Template = `
-  <wylib-pop :state="state" @submit="submit">
-    <div v-if="state.format == 'html'" v-html="state.content"/>
-    <component v-else :is="compName" :state="state.content"/>
-  </wylib-pop>
-`
-
 new Vue({
   el: '#app',
-  data() { return {
-    state:      {format: 'dialog', content: {}},
-  }},
-  template: Template,
-  components: { 'wylib-pop': Wylib.Popup, 'wylib-dialog': Wylib.Dialog, 'wylib-strdoc': Wylib.StructDoc},
-  computed: {
-    compName: function() {		//What standard component will we use
-      if (!this.state.format || this.state.format == 'html') return null
-      if (this.state.format.includes('-')) return this.state.format
-      return 'wylib-' + this.state.format
-    },
-  },
-
-  methods: {
-    kahuna: function() {			//A link to our master window
-      return window.opener || window.parent
-    },
-    submit: function(request, data) {		//If the widget we contain emits 'submit'
-//console.log("Got submit from content:", request, data)
-      this.kahuna().postMessage({request, data}, location.origin)
-    },
-  },
-
-  mounted: function() {
-//console.log("Popup mounted:", this.kahuna(), this.state)
-    this.kahuna().postMessage({request:'control'}, location.origin)	//Let parent window know we are ready to load content
-
-    window.addEventListener('message', (ev) => {	//Answer replies from the parent window
-//console.log("Popup got message:", ev.source, ev.data)
-      let { request, format, content, config } = ev.data
-        , { action } = config || {}
-      if (request != 'populate' || !format) return
-//console.log("Popup got request:", request, format, content, config)
-      this.state.format = format
-      this.state.content = content
-      if (window.opener && action) window.document.title = action.lang.title
-    })
-  },
+  template: '<wylib-pop></wylib-pop>',
+  components: { 'wylib-pop': Wylib.Popup},
 })
