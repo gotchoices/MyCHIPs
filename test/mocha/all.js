5ea0f7714b985da7190431e4459125c82ae9ea2a
Kyle Bateman <kyle@batemans.org>
2020-11-10 13:58:19 -0700

    Better timeout allowance when creating database

--- test/mocha/all.js
+++ test/mocha/all.js
@@ -1,8 +1,18 @@
 //Copyright MyCHIPs.org; See license in root of this package
 // -----------------------------------------------------------------------------
 //Run all tests in order
+const { DatabaseName, DBAdmin, MachineIP, Log } = require('../settings')
+const { exec } = require('child_process')
 
 require('./peercomm.js')
 
 require('./impexp.js')
 require('./peer.js')
+
+describe("At End", function() {
+
+  it('Drop database: ' + DatabaseName, function(done) {
+    exec('dropdb -U ' + DBAdmin + ' ' + DatabaseName)
+    done()
+  })
+});
