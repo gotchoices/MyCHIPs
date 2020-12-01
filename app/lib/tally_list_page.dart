fa3c210a5688a0d2e29da6a61bfc81ad05e7ae7a
unknown <nuffertaylor@gmail.com>
2020-11-30 19:15:19 -0700

    added searchbar and outline of tally_page

--- app/lib/tally_list_page.dart
+++ app/lib/tally_list_page.dart
@@ -10,12 +10,47 @@ class TallyListPage extends StatefulWidget {
 
 class TallyListPageState extends State<TallyListPage> {
   final List tallyList = <Tally>[];
+  bool searching = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("My Tallies")),
-      body: buildTallyList(),
+      appBar: AppBar(
+        title: searching ? TextField(
+          onChanged: (input) {
+            filterUsers(input);
+          },
+          decoration: InputDecoration(
+              icon: Icon(Icons.search, color: Colors.white,),
+              hintText: "type user here",
+              hintStyle: TextStyle(color: Colors.white)),
+          style: TextStyle(color: Colors.white),
+          cursorColor: Colors.white,
+        ) :
+        Text("My Tallies"),
+        actions: <Widget>[
+          searching ?
+          IconButton(
+              icon: Icon(Icons.cancel_outlined),
+              onPressed: () {
+                setState(() {
+                  searching = !searching;
+                });
+              }
+          ) :
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                setState(() {
+                  searching = !searching;
+                });
+              }
+          ),
+        ]
+      ),
+      body: Container(child: new Stack(children: [
+        Container(child : buildTallyList()),
+      ])),
       drawer: MainDrawer(),
     );
   }
@@ -43,5 +78,10 @@ class TallyListPageState extends State<TallyListPage> {
           ));
         }
     );
+  }
+
+  void filterUsers(input) {
+    //put logic to filter searched users here
+    print(input);
   }
 }
--- app/lib/tally_page.dart
+++ app/lib/tally_page.dart
@@ -15,15 +15,76 @@ class TallyPageState extends State<TallyPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Tally with " + widget.friend)),
+        appBar: AppBar(
+          title: Text("Tally with " + widget.friend),
+          actions: [
+            CircleAvatar(backgroundImage: new NetworkImage("https://miro.medium.com/max/450/1*W35QUSvGpcLuxPo3SRTH4w.png"),)
+          ],),
         body: buildPage(),
         drawer: MainDrawer()
     );
   }
 
   Widget buildPage() {
-    return Center(
-      child: new Text(widget.balance.toString(),  style: TextStyle(fontSize: 32))
+    return Stack(
+      children: [
+        buildBalance(),
+        buildButtons()
+
+      ]
     );
+  }
+
+  Widget buildBalance() {
+    return Padding(
+      padding: const EdgeInsets.all(15),
+      child: Align(
+        alignment: Alignment.topCenter,
+        child: Column(
+        children: [
+          Text("₵" + widget.balance.toString(),  style: TextStyle(fontSize: 75)),
+          Text("balance",  style: TextStyle(fontSize: 25)),
+        ],
+    )));
+  }
+
+  Widget buildHistory() {
+
+  }
+
+  Widget buildButtons() {
+    var maxButtonWidth = (MediaQuery.of(context).size.width) / 2.25;
+    return Row(children: [
+    Padding(
+    padding: const EdgeInsets.all(10),
+    child: Align(
+      alignment: Alignment.bottomLeft,
+      child: MaterialButton(
+        onPressed: () {
+          print("pressed 'pay'");
+        },
+        child: const Text('PAY', style: TextStyle(fontSize: 20)),
+        color: Colors.blue,
+        textColor: Colors.white,
+        elevation: 5,
+        height: 50,
+        minWidth: maxButtonWidth
+      ))),
+    Padding(
+      padding: const EdgeInsets.all(10),
+      child: Align(
+        alignment: Alignment.bottomRight,
+        child: MaterialButton(
+          onPressed: () {
+            print("pressed 'request'");
+          },
+          child: const Text('REQUEST', style: TextStyle(fontSize: 20)),
+          color: Colors.blue,
+          textColor: Colors.white,
+          elevation: 5,
+          height: 50,
+          minWidth: maxButtonWidth
+        )))
+    ]);
   }
 }
