9de141e0ac91817581a1ba0ae6370705138b9a8d
Kyle Bateman <kyle@batemans.org>
2020-12-09 21:14:14 -0700

    Docker based test/evaluation server

---
+++ build/dbsetup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Copyright MyCHIPs.org; See license in root of this package
+# -----------------------------------------------------------------------------
+#Do any initialization of postgres that may be necessary.
+#This is intended to be run inside a docker postgresql container.
+
+configfile=/var/lib/postgresql/data/pgdata/postgresql.conf
+if ! grep '^log_min_messages' $configfile; then
+  echo "Configuring Postgres for logging notices"
+  echo 'log_min_messages = notice' >>$configfile
+  echo 'log_error_verbosity = terse' >>$configfile
+fi
+
+set -e
+psql -v ON_ERROR_STOP=1 --username postgres --dbname postgres <<-EOSQL
+    create role admin with login createdb superuser createrole;
+EOSQL
