503471c92d57f58ed3b66540abead26bd9819b95
wlggraham <56554858+wlggraham@users.noreply.github.com>
2022-02-01 17:52:00 -0700

    wysemanClient changes

--- app/lib/wyseman/wysemanClient.dart
+++ app/lib/wyseman/wysemanClient.dart
@@ -1,25 +1,25 @@
+// A module to connect a dart client to a MYCHIPs server via websocket
+// see an implementation in test.dart
+
 import 'dart:convert';
 import 'dart:convert' show utf8;
+import 'dart:async';
 import 'dart:io';
 import 'dart:math';
 import 'dart:typed_data';
 import 'package:path_provider/path_provider.dart';
-import 'package:pointycastle/api.dart';
-import 'package:pointycastle/export.dart';
-import 'package:pointycastle/key_generators/api.dart';
-import 'package:pointycastle/key_generators/rsa_key_generator.dart';
-import 'package:pointycastle/random/fortuna_random.dart';
 import 'package:web_socket_channel/io.dart';
-import 'package:web_socket_channel/status.dart' as status;
-import 'package:string_to_hex/string_to_hex.dart';
-// import 'package:http/http.dart' as http;
+// import 'package:web_socket_channel/status.dart' as status;
+// import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:webcrypto/webcrypto.dart';
 
 class WysemanCredential {
   String host;
-  int port;
+  int wsPort;
   String token;
   String key;
   int keyLength;
+  String user;
 }
 
 class WysemanRequestObject {
@@ -29,15 +29,21 @@ class WysemanRequestObject {
   List<String> fields = ['id', 'std_name', 'peer_endp'];
 }
 
-class WysemanConfig {
-  List<String> DBInfo;
-  int port;
-  String CaFile;
-}
+// class WysemanConfig {
+//   List<String> dbInfo;
+//   int httpPort;
+//   String ca;
+
+//   WysemanConfig(List<String> dbInfo, int httpPort, String ca) {
+//     this.dbInfo = dbInfo;
+//     this.httpPort = httpPort;
+//     this.ca = ca;
+//   }
+// }
 
 class WysemanMessage {
   String ip;
-  double cookie;
+  String cookie;
   String userAgent;
   String date;
 
@@ -46,253 +52,202 @@ class WysemanMessage {
 }
 
 class Client {
-  String host;
-  int port;
+  int httpPort;
+  List<String> dbInfo;
   int keyLength;
-  File ca;
+  String ca;
   IOWebSocketChannel ws;
-  WysemanConfig config;
+  //WysemanConfig config;
 
-  void connect(WysemanCredential credential, var openCB) async {
-    print('connect called');
-    this.host = credential.host;
-    this.port = credential.port;
+// keylength, dbinfo, & http_port
+  Client(List<String> db, int port, String ca) {
+    this.httpPort = port;
+    this.dbInfo = db;
+    this.ca = ca;
+    //this.keyLength = keyLength;
+  }
+
+//host port, user token/key
+  connect(WysemanCredential credential, var openCB) async {
+    int wsport = credential.wsPort;
     String token = credential.token;
     String key = credential.key;
     int keyLength = credential.keyLength != null ? credential.keyLength : 2048;
+    String user = credential.user;
+    String host = credential.host;
     var rand = new Random();
-    var dbinfo = ['mychips_user', 'wylib'];
-    this.config = new WysemanConfig();
-    this.config.DBInfo = dbinfo;
-    this.config.port = credential.port;
+    String authString;
+    JsonEncoder js = new JsonEncoder();
+    Map<String, dynamic> socketHeaders = new Map();
+    var connectArray;
+
+    openWebSocket(authString, socketHeaders) {
+      //db codes to base64
+      String encodedString = js.convert(this.dbInfo);
+      List<int> bytes = utf8.encode(encodedString);
+      String dbHex = base64.encode(bytes);
+      //form query string with authstring parameter
+      String query = 'user=$user&db=$dbHex&$authString';
+      //form full connection url
+      String url = 'wss://$host:$wsport/?$query';
+      print("URL " + url);
+      //send connect array
+      //connectArray = [url, socketHeaders];
+      //open websocket connection
+      final channel =
+          IOWebSocketChannel.connect(Uri.parse(url), headers: socketHeaders);
+      // String toSend =
+      //     "{ id: 'list_cmd', action: 'select', view: 'mychips.users_v', fields: ['id', 'std_name', 'peer_endp']}";
+      // channel.sink.add(toSend);
+      // print(channel.stream);
+    }
+
+    ;
 
     if (token != null) {
       print('has token');
-    } else if (key != null) {
-      //implement later
-    } else {
-      //throw error
+
+      //Generate key pair
+
+      BigInt pe = BigInt.from(65537);
+
+      KeyPair keyPair =
+          await RsaPssPrivateKey.generateKey(keyLength, pe, Hash.sha256);
+      RsaPssPrivateKey myPrivKey = keyPair.privateKey;
+      RsaPssPublicKey myPubKey = keyPair.publicKey;
+
+      var exPub = await myPubKey.exportJsonWebKey();
+      var exPriv = await myPrivKey.exportJsonWebKey();
+
+      //Build login object
+
+      var loginTest = {
+        'login': {
+          'host': '$host',
+          'port': '$wsport',
+          'user': '${user}',
+          'privateKey': '${js.convert(exPriv)}'
+        }
+      };
+
+      final directory = await getApplicationDocumentsDirectory();
+      File myFile = await new File((directory.path + '/key.txt'));
+      myFile.writeAsString(jsonEncode(loginTest));
+
+      //build token auth string
+      authString = 'token=' +
+          token +
+          '&pub=' +
+          base64.encode(utf8.encode(js.convert(exPub)));
+
+      openWebSocket(authString, null);
+      key = loginTest['login']['privateKey'];
     }
 
-    String origin = "https://" + this.host + ":" + this.port.toString();
-    String endpoint = "/clientinfo";
+    if (key != null) {
+      Map<String, dynamic> keyJson = jsonDecode(key);
+      print(keyJson);
+      RsaPssPrivateKey myKey =
+          await RsaPssPrivateKey.importJsonWebKey(keyJson, Hash.sha256);
 
-    // print('making call to ${origin + endpoint}');
-    String current = await _localPath;
-    print(current);
-    String local = await _localPath;
+      String origin =
+          "https://" + host.toString() + ":" + this.httpPort.toString();
+      String endpoint = "/clientinfo";
 
-    // try {
-    //   print('finding file');
-    //   File f = new File(local + '/spa_ca.crt');
-    //   print('file found');
-    //   String contents = await f.readAsString();
-    //   this.config.CaFile = contents;
-    //   print('contents there');
-    // } catch (e) {
-    //   print('err');
-    // }
+      print('making call to ${origin + endpoint}');
+      HttpClient client = new HttpClient();
+      //magic line that just accepts all cert stuff
+      client.badCertificateCallback =
+          ((X509Certificate cert, String host, int port) => true);
 
-    HttpClient client = new HttpClient();
-    //magic line that just accepts all cert stuff
-    client.badCertificateCallback =
-    ((X509Certificate cert, String host, int port) => true);
-
-    try {
-      client
+      await client
           .getUrl(Uri.parse(origin + endpoint))
           .then((HttpClientRequest request) {
         request.headers.add('user-agent', 'Wyseman Websocket Client API');
         request.headers.add('cookie', rand.nextDouble().toString());
         // print('headers set');
         // print(request.headers);
-
         return request.close();
       }).then((HttpClientResponse response) {
+        final completer = Completer<void>();
         response.transform(utf8.decoder).listen((contents) async {
           print(contents);
           Map<String, dynamic> data = jsonDecode(contents);
           WysemanMessage message = new WysemanMessage();
-          JsonEncoder js = new JsonEncoder();
-          String hexString =
-          StringToHex.toHexString(js.convert(this.config.DBInfo))
-              .substring(3);
-
-          // print('data');
-          // print(data);
 
           message.ip = data['ip'];
-          message.cookie = double.parse(data['cookie']);
+          message.cookie = data['cookie'];
           message.userAgent = data['userAgent'];
           message.date = data['date'];
 
-          Uint8List utf8Thing = utf8.encode(js.convert(message));
+          //Printing to verify connection string
 
-          // print('hexstring:');
-          // print(hexString);
-          // print('uft8:');
-          // print(utf8Thing);
+          List<int> newMessage = utf8.encode(js.convert(message));
+          File newMessageFile = await new File(
+              '/tmp/.key.txt'); //remember this is in the Docker container
+          newMessageFile.writeAsBytesSync(newMessage, flush: true);
 
-          final keyGen = RSAKeyGenerator();
+          // sign bytes
 
-          var secureRandom = new FortunaRandom();
-          var random = new Random.secure();
+          Uint8List sign = await myKey.signBytes(newMessage, 128);
 
-          List<int> seeds = [];
-          for (int i = 0; i < 32; i++) {
-            seeds.add(random.nextInt(255));
-          }
-
-          secureRandom.seed(new KeyParameter(new Uint8List.fromList(seeds)));
-
-          keyGen.init(ParametersWithRandom(
-              RSAKeyGeneratorParameters(BigInt.parse('65537'), keyLength, 64),
-              secureRandom));
-
-          var keyPair = keyGen.generateKeyPair();
-
-          // print('key pairs genereated');
-
-          final signer = RSASigner(SHA256Digest(), '0609608648016503040201');
-          signer.init(
-              true, PrivateKeyParameter<RSAPrivateKey>(keyPair.privateKey));
-
-          RSASignature sig = signer.generateSignature(utf8Thing);
-          String sigString = '';
-          for (int i in sig.bytes) {
-            sigString += i.toString();
-          }
-
-          String user = 'admin';
-          String db = hexString;
-          DateTime NOW = new DateTime.now();
-          String authString = 'sign=' +
-              sigString +
-              '&date=' +
-              NOW.year.toString() +
-              '-' +
-              NOW.month.toString() +
-              '-' +
-              NOW.day.toString() +
-              'T' +
-              NOW.hour.toString() +
-              ':' +
-              NOW.minute.toString() +
-              '.' +
-              (NOW.second + NOW.millisecond).toString() +
-              'Z';
-
-          String queryString = 'user=$user&db=$db&$authString';
-          String url = 'wss://$host:$port?$queryString';
-          print('url');
-          print(url);
-
-          Map<String, dynamic> socketHeaders = new Map();
           socketHeaders['user-agent'] = 'Wyseman Websocket Client API';
           socketHeaders['cookie'] = message.cookie;
+          authString = 'sign=' +
+              base64Url.encode(sign).replaceAll('==', '') +
+              '&date=' +
+              message.date;
 
-          try {
-            print('finding file');
-            File f = new File(local + '/spa_ca.crt');
-            String contents = await f.readAsString();
-            this.config.CaFile = contents;
-            socketHeaders['ca'] = contents;
-          } catch (e) {
-            print('err');
-            await writeSPA();
-          }
-
-          try {
-            // final channel = IOWebSocketChannel.connect(Uri.parse(url),
-            //     headers: socketHeaders);
-            // channel.stream.listen((event) {
-            //   channel.sink.add('recieved');
-            //   channel.sink.close(status.goingAway);
-            // });
-            //
-            SecurityContext wsContext = new SecurityContext();
-            AsciiDecoder asciiBoi = new AsciiDecoder();
-            wsContext.setTrustedCertificates(local + '/spa_ca.crt');
-            /*
-             TODO:
-              THIS IS THE FINAL SECTION, WHERE THE CONNECTION ULTIMATELY FAILS
-             */
-            SecureSocket s = await SecureSocket.connect(this.host, this.port,
-                context: wsContext,
-                onBadCertificate: ((X509Certificate cert) => true));
-
-            s.listen((event) {
-              print('event recived!');
-              print(asciiBoi.convert(event));
-            });
-
-            String toSend =
-                "{ id: 'list_cmd', action: 'select', view: 'mychips.users_v', fields: ['id', 'std_name', 'peer_endp']}";
-
-            // Uint8List encodedMessage = utf8.encode(toSend);
-            print(toSend.toString());
-            s.write(toSend);
-            print('write sent');
-            // WebSocket webSocket =
-            //     WebSocket.fromUpgradedSocket(s, serverSide: false);
-
-            // webSocket.listen((event) {
-            //   print('upgraded');
-            // });
-
-            // webSocket.add(toSend);
-            print('it was sent');
-          } catch (exception) {
-            print('error');
-            print(exception);
-          }
+          print("Auth String: " + authString);
+          openWebSocket(authString, socketHeaders);
+          completer.complete();
         });
+        return completer.future;
       });
-    } catch (error) {
-      print('there was an error');
-      print(error);
     }
+    return connectArray;
   }
 }
 
-Future<String> get _localPath async {
-  final directory = await getApplicationDocumentsDirectory();
-  return directory.path;
-}
 
-Future<File> writeSPA() async {
-  print("writing spa");
-  final String spaFile = "-----BEGIN CERTIFICATE-----\n" +
-      "MIIEVDCCAzygAwIBAgIUReJB6W/Rvy1WALi3IxrqCOF2JHUwDQYJKoZIhvcNAQEL\n" +
-      "BQAwgbIxHTAbBgoJkiaJk/IsZAQNDA1jaGlwcGllcy5jaGlwMRkwFwYDVQQKDBBD\n" +
-      "aGlwcGllcyBTZXJ2aWNlMRAwDgYDVQQLDAdteWNoaXBzMSEwHwYJKoZIhvcNAQkB\n" +
-      "FhJteW5hbWVAbXllbWFpbC5jb20xDzANBgNVBAcMBk15dG93bjELMAkGA1UECAwC\n" +
-      "TlkxCzAJBgNVBAYTAlVTMRYwFAYDVQQDDA1jaGlwcGllcy5jaGlwMB4XDTIxMDIx\n" +
-      "NTIwMDYzOFoXDTIzMDMwNzIwMDYzOFowgbIxHTAbBgoJkiaJk/IsZAQNDA1jaGlw\n" +
-      "cGllcy5jaGlwMRkwFwYDVQQKDBBDaGlwcGllcyBTZXJ2aWNlMRAwDgYDVQQLDAdt\n" +
-      "eWNoaXBzMSEwHwYJKoZIhvcNAQkBFhJteW5hbWVAbXllbWFpbC5jb20xDzANBgNV\n" +
-      "BAcMBk15dG93bjELMAkGA1UECAwCTlkxCzAJBgNVBAYTAlVTMRYwFAYDVQQDDA1j\n" +
-      "aGlwcGllcy5jaGlwMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxop5\n" +
-      "EjutXKFdOlNGNuAXbC9hDs+YWtf++FL4fm/xcrnKfGceSZ0EDA0fdrPydpLIGcax\n" +
-      "7chHGZC27RlRhvTGpjaCQB6ALh11e/yTG5dvt12ENoqPe9QHn6kY+63jLLOHofBW\n" +
-      "AyeNkZRtSdMbqo02I2mKAZmWjnPSjHdEliHEuk/iS5AeyeLc2Ua/jloGhK2hR7Ly\n" +
-      "rLozwGt6PN1iCrzaVWwTa7nvsjar2XcTGFORRM3McKm1n/Xxfvjy6aAz01rXD3ml\n" +
-      "WUZKOPV1KWFdLQnJBALohxQeYrLowIKdT8XNmpcgS+U7JY3TIqBHipuSceXGPeFY\n" +
-      "y4QTJqt62iRLXcYrYwIDAQABo2AwXjAdBgNVHQ4EFgQUnmXqnlSfELxqiRaUn45S\n" +
-      "sZIA2s8wHwYDVR0jBBgwFoAUnmXqnlSfELxqiRaUn45SsZIA2s8wDwYDVR0TAQH/\n" +
-      "BAUwAwEB/zALBgNVHQ8EBAMCAQYwDQYJKoZIhvcNAQELBQADggEBAIdXLG/41EzC\n" +
-      "v+plU1sRZVuIIfWzxNN49KkgRYD3tOTULKT0PIEwReWNcwEBZ3yAUV2t/3C5K70m\n" +
-      "TfD0xU3Ak4Wv2Tvte9cqvY3IOVdbUDG0anv5L8YLuaB0xhjzug2rDcmRGg6PsZft\n" +
-      "pXN67LrhHfICmXDDngj6upKX//2qJLq/4l0TkYK6lcRXEOi14IyOUu6m1hwfvVUv\n" +
-      "g5Fua9txjuYd5fUA9SKZR2jQdjo8T2+8uNKCVcoh55tfIZbfOV6OjNIZe8lBdaWm\n" +
-      "6iZVOUOOIYhB8TBcOChl3oy1om/T+f3O0yU0RIiw+fBAsXs20aPJKF7BR6yRu2gi\n" +
-      "6Y+80z9ZF20=\n" +
-      "-----END CERTIFICATE-----\n";
 
-  String dir = await _localPath;
-  File f = new File('$dir/spa_ca.crt');
-  var temp = await f.writeAsString(spaFile);
 
-  print('spa written');
-  return temp;
-}
+
+
+          // channel.stream.listen((event) {
+          //   channel.sink.add('recieved');
+          //   channel.sink.close(status.goingAway);
+          // });
+
+          //SecurityContext wsContext = new SecurityContext();
+          //AsciiDecoder asciiBoi = new AsciiDecoder();
+          //wsContext.setTrustedCertificates(local + '/spa_ca.crt');
+          /*
+             TODO:
+              THIS IS THE FINAL SECTION, WHERE THE CONNECTION ULTIMATELY FAILS
+             */
+          // SecureSocket s = await SecureSocket.connect(this.host, this.port,
+          //     context: wsContext,
+          //     onBadCertificate: ((X509Certificate cert) => true));
+
+          // s.listen((event) {
+          //   print('event recived!');
+          //   print(asciiBoi.convert(event));
+          // });
+
+          // String toSend =
+          //     "{ id: 'list_cmd', action: 'select', view: 'mychips.users_v', fields: ['id', 'std_name', 'peer_endp']}";
+
+          // Uint8List encodedMessage = utf8.encode(toSend);
+          // print(toSend.toString());
+          // s.write(toSend);
+          //print('write sent');
+          // WebSocket webSocket =
+          //     WebSocket.fromUpgradedSocket(s, serverSide: false);
+
+          // webSocket.listen((event) {
+          //   print('upgraded');
+          // });
+
+          // webSocket.add(toSend);
+          //print('it was sent');
