e38cc351c429dd087c3201fe3a74c0d510756520
Nathan Allan <nate@digithought.com>
2022-02-24 23:57:03 -0700

    Launch test page.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import './view/home_page.dart';
 import 'managers/connection/connection_manager.dart';
 import 'managers/host/host_manager.dart';
+import 'view/test.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   ConnectionManager().initialize(HostManager().configStream);
-  runApp(MyChips());
+  runApp(MyChipsTestPage());
 }
