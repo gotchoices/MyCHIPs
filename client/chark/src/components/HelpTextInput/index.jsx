4df9559e8049c2bd09bf54d6041fe417907248ef
Vinay Shrestha <svinay@insightworkshop.io>
2022-12-15 17:58:45 +0545

    Fetch profile language data and update tax_id, born_date and country

---
+++ client/chark/src/components/HelpTextInput/index.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Text,
+  TouchableWithoutFeedback
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Tooltip from 'react-native-walkthrough-tooltip';
+
+import { colors } from '../../config/constants';
+
+const HelpTextInput = (props) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <View style={styles.formControl}>
+      <View style={styles.labelWrapper}>
+        <Text style={styles.label}>
+          {props.label} 
+        </Text>
+
+        {
+          props.helpText && (
+            <Tooltip
+              isVisible={isVisible}
+              content={
+                <Text>{props.helpText}</Text>
+              }
+              placement="right"
+              onClose={() => setIsVisible(false)}
+            >
+              <TouchableWithoutFeedback 
+                onPress={() => setIsVisible(true)}
+              >
+                <Icon
+                  name="question-circle"
+                  size={13}
+                  color={colors.blue}
+                  style={{ marginLeft: 5 }}
+                />
+              </TouchableWithoutFeedback>
+            </Tooltip>
+          )
+        }
+
+      </View>
+
+      <TextInput 
+        style={styles.input}
+        value={props.value}
+        onChangeText={props.onChange}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  formControl: {
+    marginVertical: 10,
+  },
+  labelWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  label: {
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: colors.gray100,
+  },
+});
+
+export default HelpTextInput;
+
