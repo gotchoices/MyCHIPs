b53d5d6e224d3edac76dcd2483cbd0a18304c8cc
Kyle Bateman <kyle@batemans.org>
2023-03-04 21:23:58 -0700

    Test for translation languages

--- test/mocha/language.js
+++ test/mocha/language.js
@@ -1,13 +1,16 @@
-//General schema tests
+//Test language data dictionary
 //Copyright MyCHIPs.org; See license in root of this package
 // -----------------------------------------------------------------------------
 // TODO
 //- 
+const Fs = require('fs')
 const assert = require("assert");
 const { DBName, DBAdmin, testLog, Schema, dbClient } = require('./common')
 var log = testLog(__filename)
 const dbConfig = {database:DBName, user:DBAdmin, connect:true, log, schema:Schema}
 const SchemaList = "'mychips','json'"
+const languages = ['fin', 'spa']
+var missing = []
 
 describe("General schema tests", function() {
   var db
@@ -36,6 +39,43 @@ log.debug("Sql:", sql)
       done()
     })
   })
+
+  function checkLanguage(lang) {
+    it(`Check for untranslated tags in: ${lang}`, function(done) {
+    
+    let fields = ['fr_lang','fr_title','fr_help','to_lang','title','help','sch','tab','type','col','tag']
+      , where = `fr_lang = 'eng' and (to_lang isnull or to_lang = '${lang}') and (help isnull or title isnull)`
+      , sql = `select ${fields.join(',')} from wm.language where ${where}`
+log.debug("Sql:", sql)
+      db.query(sql, (e, res) => {if (e) done(e)
+log.debug("res:", res)
+        rows = res?.rows
+        if (rows.length > 0) {			//If missing items found, create CSV file of results
+          let format = require('@fast-csv/format').format
+            , stream = format()
+            , file = `lang-${lang}.csv`
+            , writable = Fs.createWriteStream(file)
+          stream.pipe(writable)
+          stream.write(fields)			//Header row
+          rows.forEach(row => stream.write(row))
+          stream.end()
+        }
+        missing.push(lang)			//Remember if errors were found in this language
+//        assert.equal(rows?.length, 0)
+        done()
+      })
+    })
+  }
+
+/* Disable for now  
+  languages.forEach(lang => {
+    checkLanguage(lang)
+  })
+
+  it(`Reporting on missing language tags: ${missing}`, function(done) {
+    assert.equal(missing.length, 0)
+  })
+
 /* */
   after('Disconnect from test database', function(done) {
     db.disconnect()
