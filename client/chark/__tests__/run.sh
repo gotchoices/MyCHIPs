d435560bc3981cecd4b74ec650c3ee6d201bc394
Kyle Storey <kyle@yourroll.app>
2023-08-09 15:06:35 -0400

    Add stub checklist

--- client/chark/__tests__/run.sh
+++ client/chark/__tests__/run.sh
@@ -3,7 +3,17 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 cd ..
-npm run test
+#Look through checklist
+echo "STARTING CHECKLIST"
+while read line; do
+  if [[ $line = \#* ]]
+  then
+	  echo "$line"
+  else
+	  echo "PROCESS YN FOR" "$line"
+  fi
+done < ${SCRIPT_DIR}/checklist.txt
+exit 1
 
 
 
