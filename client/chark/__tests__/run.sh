5933b72698570efa714dbdad1f54ac473e703e9d
Kyle Storey <kyle@yourroll.app>
2023-08-09 15:30:01 -0400

    Clean up debug code and prepare for deploy

--- client/chark/__tests__/run.sh
+++ client/chark/__tests__/run.sh
@@ -5,15 +5,22 @@ cd $SCRIPT_DIR
 cd ..
 #Look through checklist
 echo "STARTING CHECKLIST"
-while read line; do
+while read -u 10 line; do
   if [[ $line = \#* ]]
   then
 	  echo "$line"
   else
-	  echo "PROCESS YN FOR" "$line"
+		while read -u 1 -p "Did you check: ${line}? " yn
+		do
+								case $yn in
+						[Yy]* ) break;;
+						[Nn]* ) echo >&2 "Maunual Test Failed: ${line}"; exit 1;;
+						* ) echo "Please answer yes or no.";;
+				esac
+		done
   fi
-done < ${SCRIPT_DIR}/checklist.txt
-exit 1
+done 10< ${SCRIPT_DIR}/checklist.txt
+exit 0 #ALL TESTS PASSED PROCEED
 
 
 
