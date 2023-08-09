2043fcd0a2243dee0bf2b8e18d8bef472682ff7d
Kyle Storey <kyle@yourroll.app>
2023-08-09 15:32:02 -0400

    Remove early debug exit

--- client/chark/__tests__/run.sh
+++ client/chark/__tests__/run.sh
@@ -4,7 +4,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 cd ..
 #Look through checklist
-echo "STARTING CHECKLIST"
 while read -u 10 line; do
   if [[ $line = \#* ]]
   then
