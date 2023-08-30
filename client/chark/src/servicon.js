0ea578b3e7ff85a51d5c899a44e6eb12aff00915
Ireshm13 <miresh@insightworkshop.io>
2023-08-30 20:15:57 +0545

    Replace server connected message on the top

--- client/chark/src/servicon.js
+++ client/chark/src/servicon.js
@@ -4,49 +4,71 @@
 //X- Make it display connection status
 //- Embelish with icons/styles
 //- Clicking on it will toggle connection status (is this needed?)
-//- 
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, Button, TouchableWithoutFeedback } from 'react-native';
+//-
+
+import React, {useState} from 'react';
+import {View, StyleSheet, TouchableWithoutFeedback} from 'react-native';
 
 import useSocket from './hooks/useSocket';
 
-import CenteredModal from './components/CenteredModal';
+import {colors} from './config/constants';
 import TokenDebug from './components/TokenDebug';
+import CenteredModal from './components/CenteredModal';
 
 const ServIcon = () => {
-  const { status } = useSocket();
+  const ServerStatus = {
+    connected: 'Server Connected',
+    connecting: 'Connecting Server...',
+    disconnected: 'Server Disconnected',
+  };
+
+  const {status} = useSocket();
   const [isModalVisible, setIsModalVisible] = useState(false);
 
   const onCancel = () => {
     setIsModalVisible(false);
-  }
+  };
+
+  const ServerStatusCircle = props => {
+    return <View style={styles.circle(props.color)} />;
+  };
+
+  const getStatusIcon = () => {
+    switch (status) {
+      case ServerStatus.connected:
+        return <ServerStatusCircle color={colors.connected} />;
+
+      case ServerStatus.connecting:
+        return <ServerStatusCircle color={colors.connecting} />;
+
+      case ServerStatus.disconnected:
+        return <ServerStatusCircle color={colors.disconnected} />;
+
+      default:
+        return <ServerStatusCircle color={colors.connecting} />;
+    }
+  };
 
   return (
     <>
       <View>
         <TouchableWithoutFeedback
           testID="serverIcon"
-          onPress={() => setIsModalVisible(true)}
-        >
-          <Text style={{ paddingLeft: 14, paddingVertical: 5 }} testID="appStatus">
-            {status}
-          </Text>
+          onPress={() => setIsModalVisible(true)}>
+          <View style={styles.wrapper} testID="appStatus">
+            {getStatusIcon()}
+          </View>
         </TouchableWithoutFeedback>
       </View>
 
-      <CenteredModal
-        isVisible={isModalVisible}
-        onClose={onCancel}
-      >
-        <TokenDebug 
-          onCancel={onCancel}
-        />
+      <CenteredModal isVisible={isModalVisible} onClose={onCancel}>
+        <TokenDebug onCancel={onCancel} />
       </CenteredModal>
     </>
-  )
-}
+  );
+};
 
-  /*
+/*
 export default class ServIcon extends Component {
   constructor(props) {
     super(props)
@@ -66,14 +88,21 @@ console.log('Connection address:', addr)
 }
 */
 
-//const styles = StyleSheet.create({
-//  buttonBox: {
-//    alignItems: 'center',
-//    backgroundColor: '#e0e0e0',
-//    borderRadius: 8,
-//    flex: 1,
-//  },
-//})
-//
+const styles = StyleSheet.create({
+  circle: color => ({
+    width: 10,
+    height: 10,
+    borderRadius: 8,
+    borderColor: color,
+    backgroundColor: color,
+  }),
+  wrapper: {
+    padding: 10,
+    paddingHorizontal: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+});
 
 export default ServIcon;
