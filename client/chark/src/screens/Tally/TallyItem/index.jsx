f6b8ea1034ec8d777b84dfc9094f621f2776a022
Ireshm13 <miresh@insightworkshop.io>
2024-04-25 23:36:48 +0545

    new tally item UI

--- client/chark/src/screens/Tally/TallyItem/index.jsx
+++ client/chark/src/screens/Tally/TallyItem/index.jsx
@@ -1,24 +1,28 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useEffect, useMemo, useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
 
-import { colors } from '../../../config/constants';
-import { round } from '../../../utils/common';
+import {colors} from '../../../config/constants';
+import {round} from '../../../utils/common';
 
 import Avatar from '../../../components/Avatar';
-import { ChitIcon } from '../../../components/SvgAssets/SvgAssets';
-import { formatRandomString } from '../../../utils/format-string';
+import {ChitIcon} from '../../../components/SvgAssets/SvgAssets';
+import {formatRandomString} from '../../../utils/format-string';
 import useMessageText from '../../../hooks/useMessageText';
+import {getDecimalValue, getIntegerValue} from '../../../utils/user';
 
-const TallyItem = (props) => {
+const TallyItem = props => {
   const tally = props.tally;
   const net = round((tally?.net ?? 0) / 1000, 3);
   const pendingNet = tally?.net_pc ? round(tally.net_pc / 1000, 3) : 0;
   const convertedNet = round(net * props.conversionRate, 2);
   const partCert = tally?.part_cert;
-  const partName = partCert?.type === 'o'
-    ? `${partCert?.name}`
-    : `${partCert?.name?.first}${partCert?.name?.middle ? ' ' + partCert?.name?.middle + ' ' : ''} ${partCert?.name?.surname}`
+  const partName =
+    partCert?.type === 'o'
+      ? `${partCert?.name}`
+      : `${partCert?.name?.first}${
+          partCert?.name?.middle ? ' ' + partCert?.name?.middle + ' ' : ''
+        } ${partCert?.name?.surname}`;
 
   const hasPendingChit = !!tally?.net_pc && net != pendingNet;
 
@@ -28,52 +32,62 @@ const TallyItem = (props) => {
     return chitMeText?.status?.values?.find(s => s.value === 'pend');
   }, [chitMeText?.status?.values]);
 
-
   return (
     <View style={styles.container}>
-
       <Avatar style={styles.avatar} avatar={props.image} />
 
-      <View style={{ flex: 1, }}>
+      <View style={{flex: 1}}>
         <Text style={styles.name}>{partName}</Text>
-        <Text style={[styles.description, { marginTop: 4 }]}>
+        <Text style={[styles.description, {marginTop: 4}]}>
           {partCert?.chad?.cid}:{formatRandomString(partCert?.chad?.agent)}
         </Text>
       </View>
 
       <View style={styles.price}>
         {hasPendingChit && (
-          <Text style={styles.pending}>
-             {pendingText?.title ?? ''} {pendingNet}
+          <View style={styles.pendingView}>
+            <Text style={styles.pending}>
+              {pendingText?.title ?? ''} chips
+            </Text>
+          </View>
+        )}
+
+        {!!props.currency && (
+          <Text style={convertedNet < 0 ? styles.dollarNeg : styles.dollar}>
+            {props.currency} {convertedNet}
           </Text>
         )}
 
         <View style={styles.mychips}>
-          <ChitIcon color={net < 0 ? colors.red : colors.green} height={18} width={12} />
-          <Text style={net < 0 ? styles.mychipsNetNeg : styles.mychipsNet}>{net}</Text>
-        </View>
+          <ChitIcon
+            color={net < 0 ? colors.red : colors.green}
+            height={18}
+            width={12}
+          />
+          <Text style={net < 0 ? styles.mychipsNetNeg : styles.mychipsNet}>
+            {getIntegerValue(net)}
+          </Text>
 
-        {
-          !!props.currency && (
-            <Text style={convertedNet < 0 ? styles.dollarNeg : styles.dollar}>
-              {convertedNet} {props.currency}
-            </Text>
-          )
-        }
+          <Text style={net < 0 ? styles.negDecimal : styles.decimal}>
+            {getDecimalValue(net)}
+          </Text>
+        </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
 const font = {
   fontFamily: 'inter',
-}
+};
 
 const mychipsNet = {
-  fontSize: 18,
+  fontSize: 22,
+  lineHeight: 22,
   fontWeight: '600',
   color: colors.green,
   fontFamily: 'inter',
+  marginHorizontal: 2,
 };
 
 const styles = StyleSheet.create({
@@ -107,10 +121,10 @@ const styles = StyleSheet.create({
   },
   pending: {
     fontSize: 8,
+    color: colors.gray700,
   },
   mychips: {
     flexDirection: 'row',
-    alignItems: 'center',
   },
   mychipsNet,
   mychipsNetNeg: {
@@ -119,23 +133,40 @@ const styles = StyleSheet.create({
   },
   dollar: {
     ...font,
-    color: colors.green,
+    color: colors.gray300,
     fontSize: 10,
     fontWeight: '600',
   },
   dollarNeg: {
     ...font,
-    color: colors.red,
+    color: colors.gray300,
     fontSize: 10,
     fontWeight: '600',
   },
-})
+  pendingView: {
+    backgroundColor: colors.white200,
+    padding: 5,
+    borderRadius: 10,
+    marginBottom:2
+  },
+  decimal: {
+    color: colors.green,
+    fontSize: 12,
+    lineHeight: 12,
+    textDecorationLine: 'underline',
+  },
+  negDecimal: {
+    fontSize: 12,
+    lineHeight: 12,
+    textDecorationLine: 'underline',
+    color: colors.red,
+  },
+});
 
 TallyItem.propTypes = {
   tally: PropTypes.object.isRequired,
   conversionRate: PropTypes.number.isRequired,
   currency: PropTypes.string,
-}
+};
 
 export default TallyItem;
-
--- client/chark/src/utils/user.js
+++ client/chark/src/utils/user.js
@@ -4,31 +4,29 @@ export function query_user(wm) {
   return new Promise((resolve, reject) => {
     wm.request(
       pktId++,
-      "select",
+      'select',
       {
-        view: "base.ent_v",
-        table: "base.curr_eid",
+        view: 'base.ent_v',
+        table: 'base.curr_eid',
         params: [],
       },
-      (data) => {
+      data => {
         resolve(data);
-      }
+      },
     );
   });
 }
 
-export const getTallyName = (item) => {
-  if (item.part_cert?.type === "o") {
+export const getTallyName = item => {
+  if (item.part_cert?.type === 'o') {
     return `${item.part_cert?.name}`;
   }
 
-  const { first, middle, surname } = item.part_cert?.name || {};
-  return `${first || ""}${middle ? " " + middle + " " : ""}${
-    surname || ""
-  }`;
+  const {first, middle, surname} = item.part_cert?.name || {};
+  return `${first || ''}${middle ? ' ' + middle + ' ' : ''}${surname || ''}`;
 };
 
-export const sortTalliesAlphabetically = (tallies) => {
+export const sortTalliesAlphabetically = tallies => {
   const sortedArray = [...tallies].sort((a, b) => {
     const tallyName1 = getTallyName(a);
     const tallyName2 = getTallyName(b);
@@ -42,17 +40,21 @@ export const sortTalliesAlphabetically = (tallies) => {
 export const sortTallies = (tallies, field, descending = false) => {
   const sorteTallies = tallies.slice().sort((a, b) => {
     const valueA =
-      field === "tally_date"
-        ? new Date(a[field])
-        : parseFloat(a[field]);
+      field === 'tally_date' ? new Date(a[field]) : parseFloat(a[field]);
 
     const valueB =
-      field === "tally_date"
-        ? new Date(b[field])
-        : parseFloat(b[field]);
+      field === 'tally_date' ? new Date(b[field]) : parseFloat(b[field]);
 
     return descending ? valueB - valueA : valueA - valueB;
   });
 
   return sorteTallies;
+};
+
+export const getIntegerValue = amount => {
+  return amount.split('.')[0];
+};
+
+export const getDecimalValue = amount => {
+  return amount.split('.')[1];
 };
